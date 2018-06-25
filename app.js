52903f6ca7ffb38589383455217b0cad80a010b0
liu xing <chn.liuxing@gmail.com>
2018-06-25 17:40:03 +0800

    test webhooks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = `${Date.now()} hello jenkins`
+  ctx.body = `${Date.now()} hello jenkins --javascipt之禅`
 })
 
 app.listen(3000)
